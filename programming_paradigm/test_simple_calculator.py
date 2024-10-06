89eaea122adda97478264ae33af8f4a089f4a058
rosine-achah <132734631+rosine-achah@users.noreply.github.com>
2024-10-06 01:42:05 -0700

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -24,7 +24,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(10, 0), 0)
 
         
-    def test_divide(self):
+    def test_division(self):
        self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(0, 1), 0)
         self.assertEqual(self.calc.divide(-6, -2), 3)
