34891ca6d7814ce9006dd080ac8b234d4719607d
rosine-achah <132734631+rosine-achah@users.noreply.github.com>
2024-10-06 01:14:12 -0700

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,23 +1,4 @@
-# class BankAccount:
-#     def __init__(self, initial_balance=0):
-#                 self._account_balance = initial_balance  
-        
-#     def deposit(self, amount):
-                    
-#                     self._account_balance += amount
-        
-#     def withdraw(self, amount):
-#                 if  amount <= self._account_balance:
-#                     self._account_balance -= amount
-#                     return True
-#                 else:
-#                     return False 
-                    
-#     def display_balance(self):
-#                 print(f"Current Balance: ${self._account_balance: }")
 
-
-# bank_account.py
 
 class BankAccount:
     def __init__(self, initial_balance=0):
