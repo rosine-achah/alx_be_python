a0d5a2df5008d85ec4a3b6f09ddc1c9cf9498ea5
rosine-achah <132734631+rosine-achah@users.noreply.github.com>
2024-10-06 01:45:41 -0700

    Update main.py

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,25 +1,3 @@
-# import sys
-# from robust_division_calculator import safe_divide
-
-# def main():
-#     if len(sys.argv) != 3:
-#         print("Usage: python main.py <numerator> <denominator>")
-#         sys.exit(1)
-
-#     numerator = sys.argv[1]
-#     denominator = sys.argv[2]
-
-#     result = safe_divide(numerator, denominator)
-#     print(result)
-
-# if __name__ == "__main__":
-#     main()
-
-
-
-
-# main.py
-
 import sys
 from robust_division_calculator import safe_divide
 
@@ -36,9 +14,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
 
 
 
